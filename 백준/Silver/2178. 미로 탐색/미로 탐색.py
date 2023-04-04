32a029692b77305502cce90904881355c254c985
Mina Seo <96722691+esthel7@users.noreply.github.com>
2023-04-05 01:45:27 +0900

    [Silver I] Title: 미로 탐색, Time: 68 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,33 +1,31 @@
 import sys
-from collections import deque
 input=sys.stdin.readline
 
 def find():
-    q=deque()
-    q.append((0,0))
-    l[0][0]=2
+    q.append([0,0,0])
     while len(q)>0:
-        n,m=q.popleft()
-        if n==N-1 and m==M-1:
-            print(l[n][m]-1)
-            break
-        if n+1<N and l[n+1][m]==1:
-            l[n+1][m]=l[n][m]+1
-            q.append((n+1,m))
-        if m+1<M and l[n][m+1]==1:
-            l[n][m+1]=l[n][m]+1
-            q.append((n,m+1))
-        if n-1>=0 and l[n-1][m]==1:
-            l[n-1][m]=l[n][m]+1
-            q.append((n-1,m))
-        if m-1>=0 and l[n][m-1]==1:
-            l[n][m-1]=l[n][m]+1
-            q.append((n,m-1))
-
+        i,j,count=q.pop(0)
+        if visited[i][j]>count+1:
+            count=count+1
+            visited[i][j]=count
+            if i==N-1 and j==M-1:
+                print(count)
+                return # 함수 나가기
+            if i+1<N and l[i+1][j]=='1':
+                q.append([i+1,j,count])
+            if j+1<M and l[i][j+1]=='1':
+                q.append([i,j+1,count])
+            if i-1>=0 and l[i-1][j]=='1':
+                q.append([i-1,j,count])
+            if j-1>=0 and l[i][j-1]=='1':
+                q.append([i,j-1,count])
 
 N,M=map(int,input().split())
-l=[0 for i in range(N)]
+l=[[]for i in range(N)]
 for i in range(N):
-    l[i]=list(map(int,input().rstrip()))
+    l[i]=list(input().rstrip())
 
+q=[]
+total=[]
+visited=[[10001 for i in range(M)] for i in range(N)]
 find()
