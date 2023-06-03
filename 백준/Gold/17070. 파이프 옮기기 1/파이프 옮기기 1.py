8172badc484638033f4924d5d8891441e012c91c
Mina Seo <96722691+esthel7@users.noreply.github.com>
2023-06-03 20:46:41 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
@@ -1,37 +1,40 @@
 import sys
 input=sys.stdin.readline
 
+def find():
+    if l[0][2]==0:
+        dp[0][2][0]=1
+        if l[1][1]==0 and l[1][2]==0:
+            dp[1][2][2]=1
+    for i in range(N):
+        for j in range(N):
+            for k in range(3):
+                if k==0 and dp[i][j][k]!=0: # 가로
+                    if j+1<N and l[i][j+1]==0:
+                        dp[i][j+1][0]+=dp[i][j][k] # 가로
+                        if i+1<N and l[i+1][j]==0 and l[i+1][j+1]==0:
+                            dp[i+1][j+1][2]+=dp[i][j][k] # 대각선
+
+                elif k==1 and dp[i][j][k]!=0: #세로
+                    if i+1<N and l[i+1][j]==0:
+                        dp[i+1][j][1]+=dp[i][j][k] # 세로
+                        if j+1<N and l[i][j+1]==0 and l[i+1][j+1]==0:
+                            dp[i+1][j+1][2]+=dp[i][j][k] # 대각선
+
+                elif k==2 and dp[i][j][k]!=0: # 대각선
+                    if j+1<N and l[i][j+1]==0:
+                        dp[i][j+1][0]+=dp[i][j][k] # 가로
+                    if i+1<N and l[i+1][j]==0:
+                        dp[i+1][j][1]+=dp[i][j][k] # 세로
+                        if j+1<N and l[i][j+1]==0 and l[i+1][j+1]==0:
+                            dp[i+1][j+1][2]+=dp[i][j][k] # 대각선
+
 N=int(input())
-l=[[]]
+l=[]
+
 for i in range(N):
-    a=[0]
-    a+=(map(int,input().split()))
-    l.append(a)
+    l.append(list(map(int,input().split())))
 
-dp=[[[0 for i in range(N+1)]for i in range(N+1)]for i in range(3)]
-# 처음은 0 가로, 1 세로, 2 대각선 구분 위함
-
-dp[0][1][2]=1 # 가로이며, (1,2)에 파이프 있음
-
-for i in range(1,N+1):
-    for j in range(1,N+1):
-        if l[i][j]!=0:
-            continue
-
-        # 가로
-        dp[0][i][j]+=dp[0][i][j-1] # 옆
-        if i-1>=1 and l[i-1][j]==0 and j-1>=1 and l[i][j-1]==0: # 대각선
-            dp[2][i][j]+=dp[0][i-1][j-1]
-
-        # 세로
-        dp[1][i][j]+=dp[1][i-1][j] # 아래
-        if i-1>=1 and l[i-1][j]==0 and j-1>=1 and l[i][j-1]==0: # 대각선
-            dp[2][i][j]+=dp[1][i-1][j-1]
-
-        # 대각선
-        dp[0][i][j]+=dp[2][i][j-1] # 옆
-        dp[1][i][j]+=dp[2][i-1][j] # 아래
-        if i-1>=1 and l[i-1][j]==0 and j-1>=1 and l[i][j-1]==0: # 대각선
-            dp[2][i][j]+=dp[2][i-1][j-1]
-
-print(dp[0][N][N]+dp[1][N][N]+dp[2][N][N])
+dp=[[[0,0,0]for i in range(N)]for i in range(N)]
+find()
+print(dp[N-1][N-1][0]+dp[N-1][N-1][1]+dp[N-1][N-1][2])
