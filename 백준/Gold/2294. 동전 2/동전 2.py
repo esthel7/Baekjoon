950611fbfafc274dcda501c9475a3ad7791c9ef8
Mina Seo <96722691+esthel7@users.noreply.github.com>
2023-06-07 01:33:34 +0900

    [Gold V] Title: 동전 2, Time: 196 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.py
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -1,29 +1,20 @@
 import sys
 input=sys.stdin.readline
 
-def find():
-    for i in range(len(l)):
-        j=l[i]
-        if j<k+1:
-            if dp[j]==-1 or dp[j]>1:
-                dp[j]=1
-            for j in range(l[i]+1,k+1):
-                if j%l[i]==0:
-                    if dp[j]>dp[j-l[i]]+1 or dp[j]==-1:
-                        dp[j]=dp[j-l[i]]+1
-                elif dp[j-l[i]]!=-1:
-                    if dp[j]>dp[j-l[i]]+1 or dp[j]==-1:
-                        dp[j]=dp[j-l[i]]+1
-            # print(dp)
-
 n,k=map(int,input().split())
 l=[]
 for i in range(n):
     l.append(int(input()))
 
-l=list(set(l))
+dp=[10001 for i in range(k+1)]
+dp[0]=0
 
-dp=[-1 for i in range(k+1)]
-dp[0]=1
-find()
-print(dp[k])
+for i in range(n):
+    for j in range(k+1-l[i]):
+        if dp[j+l[i]]>dp[j]+1:
+            dp[j+l[i]]=dp[j]+1
+
+if dp[-1]==10001:
+    print(-1)
+else:
+    print(dp[-1])
