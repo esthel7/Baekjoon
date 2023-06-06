412e080441abda3d30683aaad500293ade27e20b
Mina Seo <96722691+esthel7@users.noreply.github.com>
2023-06-07 04:07:09 +0900

    [Silver V] Title: 수들의 합 5, Time: 3968 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.py
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.py
@@ -3,23 +3,16 @@ input=sys.stdin.readline
 
 N=int(input())
 
-i=0
-j=0
 count=0
-now=0
-while j<=N:
-    if now>N:
-        now-=i
-        i+=1
-    elif now<N:
-        j+=1
-        now+=j
-    else:
-        # print('append',i,j)
-        count+=1
-        now-=i
-        i+=1
-        j+=1
-        now+=j
+total=0
+Min=1
+for i in range(1,N+1):
+    if total<N:
+        total+=i
+        while total>=N:
+            if total==N:
+                count+=1
+            total-=Min
+            Min+=1
 
 print(count)
