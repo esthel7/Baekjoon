ed91c5b9f37df9d48afc8c487777c1bd52642d19
Mina Seo <96722691+esthel7@users.noreply.github.com>
2023-09-10 03:28:17 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,30 +1,29 @@
-def solution(n):
-    answer = 0
-    v=1
-    cnt=0
+def n3(n):
+    a=1
+    num=0
     l=[]
-    while n!=0:
-        if n==v:
+    while True:
+        if n==0:
+            break
+        if a>n:
             if len(l)==0:
-                l=[0 for i in range(cnt+1)]
-            l[cnt]=1
-            n=0
-        elif n<v:
-            if len(l)==0:
-                l=[0 for i in range(cnt)]
-            if n//(v/3)==2:
-                l[cnt-1]=2
-                n=n-v/3*2
-            else:
-                l[cnt-1]=1
-                n=n-v/3
-            v=1
-            cnt=0
+                l=[0 for i in range(num)]
+            l[num-1]+=1
+            n=n-a/3
+            a=1
+            num=0
         else:
-            v*=3
-            cnt+=1
-    
-    L=len(l)
-    for i in range(L):
-        answer+=pow(3,i)*l[L-1-i]
+            a*=3
+            num+=1
+    return l
+
+def n10(l):
+    num=0
+    for i in range(len(l)):
+        num+=pow(3,i)*l[len(l)-i-1]
+    return num
+
+def solution(n):
+    newN=n3(n)
+    answer=n10(newN)
     return answer
