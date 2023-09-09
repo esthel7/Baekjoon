febadc7c85a4c63b965e5f5c87dea54cbd4716a8
Mina Seo <96722691+esthel7@users.noreply.github.com>
2023-09-10 03:11:08 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.51 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -1,8 +1,10 @@
 def solution(numbers):
     answer = []
     num=len(numbers)
+    
     for i in range(num):
         for j in range(i+1,num):
             answer.append(numbers[i]+numbers[j])
+    
     answer=sorted(list(set(answer)))
     return answer
