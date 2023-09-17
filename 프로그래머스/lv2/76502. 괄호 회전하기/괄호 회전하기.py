e8831ff200130d8892d5c9d96b0f72cc51f2e5ce
Mina Seo <96722691+esthel7@users.noreply.github.com>
2023-09-17 22:17:47 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,32 +1,27 @@
-def solution(s):
-    answer = -1
-    s=list(s)
-    s=s+s
-    n=len(s)//2
+def run(s):
+    start=['(','{','[']
     l=[]
-    sMax=0
-    for i in range(n):
-        flag=0
-        for j in range(i,i+n,1):
-            if s[j]=='(' or s[j]=='[' or s[j]=='{':
-                l.append(s[j])
-            elif s[j]==')' or s[j]==']' or s[j]=='}':
-                if len(l)==0:
-                    flag=1
-                    break
-                if l[-1]=='(' and s[j]==')' or l[-1]=='[' and s[j]==']' or l[-1]=='{' and s[j]=='}':
-                    l.pop(-1)
-                    if len(l)==0:
-                        sMax+=1
-                else:
-                    flag=1
-                    break
-        if flag==1:
-            sMax=0
-            continue
+    for i in range(len(s)):
+        if s[i] in start:
+            l.append(s[i])
         else:
-            answer=sMax
-            break
-    if flag==1:
-        answer=0
-    return answer
+            if len(l)>0:
+                if s[i]==')' and l[-1]=='(' or s[i]==']' and l[-1]=='[' or s[i]=='}' and l[-1]=='{':
+                    l.pop(-1)
+                    continue
+                else:
+                    return 0
+            else:
+                return 0
+    if len(l)>0:
+         return 0
+    return 1
+
+def solution(s):
+    num=0
+    s=list(s)
+    for i in range(len(s)):
+        num+=run(s)
+        a=s.pop(0)
+        s.append(a)
+    return num
