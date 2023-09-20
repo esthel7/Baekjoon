de7c5b3fab831585938ebddcef548f6356a06873
Mina Seo <96722691+esthel7@users.noreply.github.com>
2023-09-21 00:45:07 +0900

    [level 2] Title: n^2 배열 자르기, Time: 20.65 ms, Memory: 17.7 MB -BaekjoonHub

--- 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
+++ 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
@@ -1,10 +1,19 @@
 def solution(n, left, right):
     answer = []
-    for i in range(left,right+1,1):
-        x=i//n
-        y=i%n
-        if y<=x:
-            answer.append(x+1)
-        else:
-            answer.append(y+1)
+    
+    leftF=left//n
+    leftL=left%n
+    rightF=right//n
+    rightL=right%n
+    if leftF==rightF:
+        for i in range(leftL,rightL+1):
+            answer.append(max(leftF+1,i+1))
+        return answer
+    for i in range(leftL,n):
+        answer.append(max(leftF+1,i+1))
+    for i in range(leftF+1,rightF+1):
+        for j in range(n):
+            if i==rightF and j>rightL:
+                break
+            answer.append(max(i+1,j+1))
     return answer
