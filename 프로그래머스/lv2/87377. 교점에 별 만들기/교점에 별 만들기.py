a8bef9663d41485f6b5160ec9429bf1dd8c10f20
Mina Seo <96722691+esthel7@users.noreply.github.com>
2023-09-22 03:53:51 +0900

    [level 2] Title: 교점에 별 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/87377. 교점에 별 만들기/교점에 별 만들기.py
+++ 프로그래머스/lv2/87377. 교점에 별 만들기/교점에 별 만들기.py
@@ -1,58 +1,33 @@
 def solution(line):
-    
-    def check(a,b):
-        if a==0:
-            return 0
-        if b==0:
-            return 0.1
-        if a//b==a/b:
-            return a//b
-        return 0.1
-    
-    def cal(x,y,maxX,minX,maxY,minY):
-        if maxX==0.1 or x>maxX:
-            maxX=x
-        if minX==0.1 or x<minX:
-            minX=x
-        if maxY==0.1 or y>maxY:
-            maxY=y
-        if minY==0.1 or y<minY:
-            minY=y
-        return maxX,minX,maxY,minY
-    
-    def star(maxX,minX,maxY,minY):
-        X=maxX-minX+1
-        Y=maxY-minY+1
-        dots=[['.' for i in range(X)]for i in range(Y)]
-        while len(dot):
-            x,y=dot.pop(0)
-            dots[maxY-y][x-minX]='*'
-
-        answer=[[]for i in range(Y)]
-        for i in range(Y):
-            answer[i]=''.join(dots[i])
-        return answer
-
     answer = []
-    n=len(line)
-    dot=[]
-    maxX,minX,maxY,minY=0.1,0.1,0.1,0.1
-    for i in range(n):
-        for j in range(i+1,n):
+    for i in range(len(line)):
+        for j in range(i+1,len(line)):
             A=line[i][0]
             B=line[i][1]
             E=line[i][2]
             C=line[j][0]
             D=line[j][1]
             F=line[j][2]
-            x=check(B*F-E*D,A*D-B*C)
-            if x==0.1:
+            if A*D-B*C==0:
                 continue
-            y=check(E*C-A*F,A*D-B*C)
-            if y==0.1:
-                continue
-            dot.append([x,y])
-            maxX,minX,maxY,minY=cal(x,y,maxX,minX,maxY,minY)
+            if (B*F-E*D)%(A*D-B*C)==0:
+                x=(B*F-E*D)//(A*D-B*C)
+                if (E*C-A*F)%(A*D-B*C)==0:
+                    y=(E*C-A*F)//(A*D-B*C)
+                    answer.append([x,y])
+            continue
 
-    answer=star(maxX,minX,maxY,minY)
-    return answer
+    answer.sort() # x 작은값~큰값
+    minX=answer[0][0]
+    maxX=answer[len(answer)-1][0]
+    answer.sort(key=lambda x:x[1]) # y 작은값~큰값
+    minY=answer[0][1]
+    maxY=answer[len(answer)-1][1]
+    result=[['.' for i in range(maxX-minX+1)] for i in range(maxY-minY+1)]
+    for i in range(len(answer)):
+        x=answer[i][0]-minX
+        y=maxY-answer[i][1]
+        result[y][x]='*'
+    for i in range(len(result)):
+        result[i]=''.join(result[i])
+    return result
