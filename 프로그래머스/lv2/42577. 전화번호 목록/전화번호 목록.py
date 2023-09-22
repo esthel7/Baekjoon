20b69a3fdc781f1e8b6969fa128601214940d993
Mina Seo <96722691+esthel7@users.noreply.github.com>
2023-09-23 01:26:04 +0900

    [level 2] Title: 전화번호 목록, Time: 119.24 ms, Memory: 28 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,11 +1,8 @@
 def solution(phone_book):
-    # 있으면 false
     answer = True
-    n=len(phone_book)
-    phone_book=sorted(phone_book)
-    for i in range(n-1):
-        I=min(len(phone_book[i]),len(phone_book[i+1]))
-        if phone_book[i][:I]==phone_book[i+1][:I]:
-            answer=False
-            return answer
+    phone_book.sort()
+    for i in range(len(phone_book)-1):
+        minLen=min(len(phone_book[i]),len(phone_book[i+1]))
+        if phone_book[i][0:minLen]==phone_book[i+1][0:minLen]:
+            return False
     return answer
