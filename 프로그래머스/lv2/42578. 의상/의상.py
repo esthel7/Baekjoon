24fe2830614b040eaaf6f2bfb8b4f5cfbeaab92e
Mina Seo <96722691+esthel7@users.noreply.github.com>
2023-09-23 03:53:17 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.py
+++ 프로그래머스/lv2/42578. 의상/의상.py
@@ -1,35 +1,18 @@
 def solution(clothes):
-    
-    def cal(start,C,now):
-        for i in range(start,C):
-            now*=length[i]
-            add.append(now)
-            cal(i+1,C,now)
-            now//=length[i]
-
-    answer = 0
-    clo={}
-    groups=[]
-    n=len(clothes)
-    for i in range(n):
-        wear,group=clothes[i][0],clothes[i][1]
-        if group in clo:
-            clo[group].append(wear)
+    clothes.sort(key=lambda x:x[1])
+    lists=[]
+    lists.append([clothes[0][0]])
+    for i in range(1,len(clothes)):
+        if clothes[i-1][1]==clothes[i][1]:
+            lists[len(lists)-1].append(clothes[i][0])
         else:
-            clo[group]=[wear]
-            groups.append(group) # key
+            lists.append([clothes[i][0]])
+
+    # 해당 종류의 옷을 (여러개) 입거나 안입거나
+    # 모든 종류들의 갯수 + 1 을 다 곱한 후 아무것도 안입는 경우 하나 빼기
+    total=1
+    for i in range(len(lists)):
+        total*=len(lists[i])+1
+    return total-1
     
-    C=len(clo)
-    length=[0 for i in range(C)]
-    answer=1
-    for i in range(C):
-        length[i]=len(clo[groups[i]])
-        answer*=length[i]+1
-
-#     add=[]
-#     cal(0,C,1)
-
-#     for j in range(len(add)):
-#         answer+=add[j]
-                
-    return answer-1
+    # return total
