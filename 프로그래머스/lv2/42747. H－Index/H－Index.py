d220ce9b935b15833bf34be57a0ebb3450546a20
Mina Seo <96722691+esthel7@users.noreply.github.com>
2023-09-23 21:53:11 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -1,13 +1,14 @@
 def solution(citations):
-    answer = 0
-    n=len(citations)
-    citations=sorted(citations,reverse=True)
-    for i in range(n):
-        # citations[i]번 이상 인용된 횟수가 i+1번
-        # citations[i+1]번 이상 인용된 횟수가 i+2번
-        if citations[i]>=i+1:
-            answer=i+1
-        else:
-            break
-
-    return answer
+    total=len(citations)
+    maxNum=-1
+    for i in range(max(citations)+1):
+        up=0
+        down=0
+        for j in range(total):
+            if citations[j]>=i:
+                up+=1
+            else:
+                down+=1
+        if up>=i:
+            maxNum=i
+    return maxNum
