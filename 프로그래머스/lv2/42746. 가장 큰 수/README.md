0c094d93a3b2793ff0d714ef88f4c55c96613e23
Mina Seo <96722691+esthel7@users.noreply.github.com>
2023-09-23 21:33:02 +0900

    [level 2] Title: 가장 큰 수, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/README.md
+++ 프로그래머스/lv2/42746. 가장 큰 수/README.md
@@ -1,6 +1,6 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746#) 
 
 ### 성능 요약
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-Empty
+정확성: 100.0<br/>합계: 100.0 / 100.0
 
 ### 문제 설명
 
--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,13 +1,6 @@
 def solution(numbers):
-    answer = ''
-    n=len(numbers)
-    for i in range(n):
-        numbers[i]=list(str(numbers[i]))
-    numbers=sorted(numbers, key=lambda x:x*3, reverse=True)
-
-    new=[]
-    for i in range(n):
-        new+=numbers[i]
-    answer=''.join(new)
-    answer=str(int(answer)) # 0000 방지 위함
-    return answer
+    numbers.sort(key=lambda x:list(str(x))*3,reverse=True)
+    answer=''
+    for i in range(len(numbers)):
+        answer+=''.join(list(str(numbers[i])))
+    return str(int(answer))
