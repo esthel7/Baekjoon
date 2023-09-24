0b3a425f79f2b9d791f52fd11c8eb33d97fa0749
Mina Seo <96722691+esthel7@users.noreply.github.com>
2023-09-24 22:08:01 +0900

    [level 2] Title: 타겟 넘버, Time: 14.69 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,14 +1,26 @@
-def solution(numbers, target):
-    def find(i,now,n):
-        if i==n:
-            if now==target:
-                l.append(1)
+def go(l,numbers,target):
+    total=0
+    for i in range(len(numbers)):
+        if l[i]<0:
+            total-=numbers[i]
         else:
-            find(i+1,now+numbers[i],n)
-            find(i+1,now-numbers[i],n)
-    answer = 0
-    n=len(numbers)
+            total+=numbers[i]
+    if total==target:
+        result[0]+=1
+
+def make(l,index,numbers,target):
+    if index==len(numbers)+1:
+        go(l,numbers,target)
+    else:
+        l.append(index)
+        make(l,index+1,numbers,target)
+        l.pop(-1)
+        l.append((-1)*index)
+        make(l,index+1,numbers,target)
+        l.pop(-1)
+
+result=[0]
+def solution(numbers, target):
     l=[]
-    find(0,0,n)
-    answer=len(l)
-    return answer
+    make(l,1,numbers,target)
+    return result[0]
