a0afd24df9797f27e1108d9017350486ea370f8f
Mina Seo <96722691+esthel7@users.noreply.github.com>
2024-01-04 19:35:39 +0900

    [Silver V] Title: 방 번호, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.py
+++ 백준/Silver/1475. 방 번호/방 번호.py
@@ -1,27 +1,26 @@
 N=list(input())
 cnt=1
-now=[1 for i in range(10)]
+flag=7
+now=[7 for i in range(10)]
 for i in range(len(N)):
   num=int(N[i])-1
   if num==5 or num==8:
-    if now[5]>=1:
+    if now[5]>=flag:
       now[5]-=1
       continue
-    elif now[8]>=1:
+    elif now[8]>=flag:
       now[8]-=1
       continue
     else:
       cnt+=1
-      for j in range(10):
-        now[j]+=1
+      flag-=1
       now[num]-=1
   else:
-    if now[num]>=1:
+    if now[num]>=flag:
       now[num]-=1
       continue
     else:
       cnt+=1
-      for j in range(10):
-        now[j]+=1
+      flag-=1
       now[num]-=1
 print(cnt)
