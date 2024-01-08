3dced2917b72184adb6ab509b1d6fb254c820ce9
Mina Seo <96722691+esthel7@users.noreply.github.com>
2024-01-08 22:01:09 +0900

    [Silver II] Title: 유기농 배추, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,63 +1,33 @@
 import sys
 input=sys.stdin.readline
 
-def make(u):
-    for i in range(K):
-        x,y=l[i]
-        u[y][x]=1
-
-def find(u):
-    total=0 # 배추
-    cnt=0 # 지렁이
-    a=0
-    b=0
-    yes=[]
-    no=[]
-    if u[a][b]==0:
-        no.append((a,b+1))
-        no.append((a+1,b))
-    else:
-        yes.append((a,b+1))
-        yes.append((a+1,b))
-        total=total+1
-        cnt=cnt+1
-    visited[a][b]=1
-
-    while total!=K:
-        flag=0
-        if len(yes)>0:
-            a,b=yes.pop(0)
-            flag=1
-        else:
-            a,b=no.pop(0)
-        
-        if a<N and b<M and a>=0 and b>=0 and visited[a][b]!=1:
-            if u[a][b]==1:
-                total=total+1
-                if flag==0:
-                    cnt=cnt+1
-                yes.append((a,b-1))
-                yes.append((a-1,b))
-                yes.append((a,b+1))
-                yes.append((a+1,b))
-            else:
-                no.append((a,b-1))
-                no.append((a-1,b))
-                no.append((a,b+1))
-                no.append((a+1,b))
-
-            visited[a][b]=1
-    print(cnt)
+def change(l,x,y,M,N):
+  q=[[x,y]]
+  while q:
+    [x,y]=q.pop()
+    if x>=N or y>=M or x<0 or y<0:
+      continue
+    if l[x][y]==1:
+      l[x][y]=0
+      q.append([x-1,y])
+      q.append([x+1,y])
+      q.append([x,y-1])
+      q.append([x,y+1])
+  return l
 
 
 T=int(input())
-for _ in range(T):
-    M,N,K=map(int,input().split())
-    l=[]
-    for i in range(K):
-        x,y=map(int,input().split())
-        l.append((x,y))
-    u=[[0 for a in range(M)]for b in range(N)]
-    visited=[[0 for a in range(M)]for b in range(N)]
-    make(u)
-    find(u)
+for i in range(T):
+  M,N,K=map(int,input().split())
+  l=[[0 for a in range(M)] for b in range(N)]
+  for j in range(K):
+    x,y=map(int,input().split())
+    l[y][x]=1
+  
+  cnt=0
+  for i in range(N):
+    for j in range(M):
+      if l[i][j]==1:
+        l=change(l,i,j,M,N)
+        cnt+=1
+  print(cnt)
