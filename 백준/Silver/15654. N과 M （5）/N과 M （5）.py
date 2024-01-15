24a33946b1242d8055311a008de000c9a9d9f199
Mina Seo <96722691+esthel7@users.noreply.github.com>
2024-01-15 22:33:52 +0900

    [Silver III] Title: N과 M (5), Time: 256 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -0,0 +1,20 @@
+def find(N,M,num):
+  if len(num)==M:
+    for answer in num:
+      print(answer,end=' ')
+    print()
+    return
+
+  for i in range(N):
+    if l[i] in num:
+      continue
+    num.append(l[i])
+    find(N,M,num)
+    num.pop()
+
+
+N,M=map(int,input().split())
+l=list(map(int,input().split()))
+l.sort()
+
+find(N,M,[])
