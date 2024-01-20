c610aa88db73451a5aec7bbaa609b47f29c571f8
Mina Seo <96722691+esthel7@users.noreply.github.com>
2024-01-20 23:40:50 +0900

    [Silver III] Title: N과 M (1), Time: 244 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,23 +1,17 @@
-import sys
-input=sys.stdin.readline
-
-def find():
-    if len(stack)==M:
-        for i in range(M):
-            print(stack[i],end=' ')
-        print()
-    
-    for i in range(N):
-        if visited[i]==1:
-            continue
-        visited[i]=1
-        stack.append(i+1)
-        find()
-        stack.pop()
-        visited[i]=0
+def find(N,M,num):
+  if len(num)==M:
+    for answer in num:
+      print(answer,end=' ')
+    print()
+    return
+  
+  for i in range(N):
+    if l[i] in num:
+      continue
+    num.append(l[i])
+    find(N,M,num)
+    num.pop()
 
 N,M=map(int,input().split())
-
-stack=[]
-visited=[0]*(N) # visited=[0 for i in range(N)] 대신 사용
-find()
+l=[i for i in range(1,N+1)]
+find(N,M,[])
