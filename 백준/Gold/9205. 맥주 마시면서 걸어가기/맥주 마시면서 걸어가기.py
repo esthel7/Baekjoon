4df804e2425afdba17868434518d26d06317883d
Mina Seo <96722691+esthel7@users.noreply.github.com>
2024-02-06 02:29:02 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
@@ -0,0 +1,37 @@
+import sys
+input=sys.stdin.readline
+
+# 50m에 한병
+# 빈병 버리고 새로 살 수 있고 max는 20
+
+def find(n,x,y):
+  q=[[20,x,y]]
+  while q:
+    cnt,x,y=q.pop()
+    if abs(rock[0]-x)+abs(rock[1]-y)<=50*cnt:
+      print('happy')
+      return
+    i=0
+    while i<len(l):
+      if abs(l[i][0]-x)+abs(l[i][1]-y)<=50*cnt:
+        q.append([20,l[i][0],l[i][1]])
+        l.pop(i)
+        continue
+      i+=1
+
+  print('sad')
+
+t=int(input())
+for i in range(t):
+  n=int(input())
+  l=[]
+  x,y=map(int,input().split())
+  l=[]
+  for j in range(1,n+1):
+    l.append(list(map(int,input().split())))
+  rock=list(map(int,input().split()))
+  find(n,x,y)
+  # if abs(rock[0]-x)+abs(rock[1]-y)<=n*1000:
+  #   find(n,x,y)
+  # else:
+  #   print('sad')
