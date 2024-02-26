ea2e18ca889fe939c9b30ecfa6ff6cd2d1a8adc8
Mina Seo <96722691+esthel7@users.noreply.github.com>
2024-02-26 23:40:23 +0900

    [Silver II] Title: N과 M (10), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15664. N과 M （10）/N과 M （10）.py
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -1,35 +1,28 @@
-def find(N,M,start,info,num,numbers):
-  if len(num)==M:
-    for answer in numbers:
-      print(answer,end=' ')
-    print()
-    return
+import sys
+input=sys.stdin.readline
 
+def find(start,now,num,N,M):
+  if len(now)==M:
+    check=''
+    for item in now:
+      check+=str(item)+' '
+    if check in info:
+      return
+    info[check]=True
+    print(check)
+    return
   for i in range(start,N):
     if i in num:
       continue
-    if l[i][1]==info[l[i][0]]:
-      info[l[i][0]]+=1
-      num.append(i)
-      numbers.append(l[i][0])
-      find(N,M,i+1,info,num,numbers)
-      num.pop()
-      numbers.pop()
-      info[l[i][0]]-=1
+    num.append(i)
+    now.append(l[i])
+    find(i+1,now,num,N,M)
+    num.pop()
+    now.pop()
 
-
+info={}
 N,M=map(int,input().split())
 l=list(map(int,input().split()))
 l.sort()
 
-l[0]=[l[0],1]
-info={l[0][0]:1}
-
-for i in range(1,N):
-  if l[i-1][0]==l[i]:
-    l[i]=[l[i],l[i-1][1]+1]
-  else:
-    l[i]=[l[i],1]
-    info[l[i][0]]=1
-
-find(N,M,0,info,[],[])
+find(0,[],[],N,M)
