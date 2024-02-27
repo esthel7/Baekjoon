e7a4471ff303c730c1cd91346419c5f37850b9e9
Mina Seo <96722691+esthel7@users.noreply.github.com>
2024-02-27 19:56:43 +0900

    [Silver I] Title: 포도주 시식, Time: 56 ms, Memory: 33164 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -1,18 +1,24 @@
 import sys
 input=sys.stdin.readline
 
-# 연속 3잔 불가
-
 n=int(input())
-l=[0,0,0]
+l=[]
 for i in range(n):
   l.append(int(input()))
 
-num=[[0,0,0] for i in range(n+3)]
+dp=[[0,0,0] for i in range(n)]
+dp[0][0]=l[0]
+dp[0][1]=l[0]
+if n>1:
+  dp[1][0]=dp[0][0]+l[1]
+  dp[1][1]=l[1]
+  dp[1][2]=dp[0][0]
+for i in range(2,n):
+  pprev=max(dp[i-2])
+  prev=max(dp[i-1])
+  listprev=dp[i-1][1]
+  dp[i][0]=listprev+l[i]
+  dp[i][1]=pprev+l[i]
+  dp[i][2]=prev
 
-for i in range(3,n+3):
-  num[i][0]=max(num[i-3])+l[i] # 가장 큰 전전전값 더하기
-  num[i][1]=max(num[i-2])+l[i] # 가장 큰 전전값 더하기
-  num[i][2]=max(num[i-1][0],num[i-1][1])+l[i] # 전전값이 빠진 전값 더하기
-
-print(max(max(num[n]),max(num[n+1]),max(num[n+2])))
+print(max(dp[n-1]))
