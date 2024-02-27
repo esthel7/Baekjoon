26888af0717499a2b2874dc0f0676290c6a1949d
Mina Seo <96722691+esthel7@users.noreply.github.com>
2024-02-28 01:06:15 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 79.71 ms, Memory: 19.3 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -1,45 +1,45 @@
 from collections import deque
 
-def count(q1,q2,t1,t2,l1,l2,half,cnt,maximum):
-    q=[[q1,q2,t1,t2,l1,l2,cnt]]
-    while True:
-        [q1,q2,t1,t2,l1,l2,cnt]=q.pop()
-        if cnt>maximum:
-            return -1
-        if t1==half:
-            return cnt
-        if l1==1 and t1>half:
-            return -1
-        if l2==1 and t2>half:
-            return -1
-        
-        f1=q1[0]
-        f2=q2[0]
-        if t1>t2:
-            q1.popleft()
-            q2.append(f1)
-            q.append([q1,q2,t1-f1,t2+f1,l1-1,l2+1,cnt+1])
-        else:
-            q1.append(f2)
-            q2.popleft()
-            q.append([q1,q2,t1+f2,t2-f2,l1+1,l2-1,cnt+1])
-        
+# 맨앞 pop, 뒤로 insert
 
 def solution(queue1, queue2):
+    t1=sum(queue1)
+    t2=sum(queue2)
+    n=len(queue1)*3
+
+    if (t1+t2)%2==1:
+        return -1
+    
+    goal=(t1+t2)//2
+    
     queue1=deque(queue1)
     queue2=deque(queue2)
-    len1=0
-    len2=0
-    total1=0
-    total2=0
+    
+    q=deque([[t1,t2,queue1,queue2,0]])
+    
+    while q:
+        t1,t2,q1,q2,cnt=q.popleft()
+        if cnt>n:
+            answer=-1
+            break
 
-    for i in range(len(queue1)):
-        len1+=1
-        len2+=1
-        total1+=queue1[i]
-        total2+=queue2[i]
-    half=(total1+total2)//2
-    if (total1+total2)%2!=0:
-        return -1
+        if t1==goal:
+            answer=cnt
+            break
 
-    return count(queue1,queue2,total1,total2,len1,len2,half,0,3*len1)
+        if t1>t2:
+            left=q1.popleft()
+            if left>goal:
+                answer=-1
+                break
+            q2.append(left)
+            q.append([t1-left,t2+left,q1,q2,cnt+1])
+        else:
+            left=q2.popleft()
+            if left>goal:
+                answer=-1
+                break
+            q1.append(left)
+            q.append([t1+left,t2-left,q1,q2,cnt+1])
+    
+    return answer
