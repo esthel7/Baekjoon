f91230c74ef63c7c30099b8d6029a312d9c32d8d
Mina Seo <96722691+esthel7@users.noreply.github.com>
2024-05-23 23:35:23 +0900

    [Silver IV] Title: 덱, Time: 216 ms, Memory: 12360 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.js
@@ -0,0 +1,53 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+rl.on('line', (line) => {
+  input.push(line);
+}).on('close', () => {
+  cnt = 0;
+  n = input.shift();
+  save = [];
+  answer = [];
+  for (let i = 0; i < n; i++) {
+    now = input[i].split(' ');
+    if (now.length === 2) {
+      // push
+      cnt++;
+      if (now[0] === 'push_back') save.push(now[1]);
+      else save.unshift(now[1]);
+    } else {
+      now = now[0];
+      if (now === 'pop_front') {
+        if (save.length === 0) answer.push(-1);
+        else {
+          answer.push(save.shift());
+          cnt--;
+        }
+      } else if (now === 'pop_back') {
+        if (save.length === 0) answer.push(-1);
+        else {
+          answer.push(save.pop());
+          cnt--;
+        }
+      } else if (now === 'size') answer.push(save.length);
+      else if (now === 'empty') {
+        if (save.length === 0) answer.push(1);
+        else answer.push(0);
+      } else if (now === 'front') {
+        if (save.length === 0) answer.push(-1);
+        else answer.push(save[0]);
+      } else {
+        if (save.length === 0) answer.push(-1);
+        else answer.push(save[cnt - 1]);
+      }
+    }
+  }
+  answer=answer.join('\n');
+  console.log(answer);
+  process.exit();
+});
