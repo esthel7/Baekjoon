5edc1581bce18300b065c0941171106ab065977d
Mina Seo <96722691+esthel7@users.noreply.github.com>
2024-08-09 04:28:22 +0900

    [Silver II] Title: 외판원 순회 2, Time: 84 ms, Memory: 38332 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -1,31 +1,48 @@
 import sys
+import heapq
+input=sys.stdin.readline
 
-
-def dfs(start, now, value, cnt):
-    global ans
-    if cnt == N:
-        if a[now][start]:
-            value += a[now][start]
-            if ans > value:
-                ans = value
-        return
-
-    if value > ans:
-        return
-
-    for i in range(N):
-        if not visited[i] and a[now][i]:
-            visited[i] = 1
-            dfs(start, i, value + a[now][i], cnt + 1)
-            visited[i] = 0
-
-
-N = int(input())
-a = [list(map(int, input().split()))for _ in range(N)]
-ans = sys.maxsize
-visited = [0] * N
+N=int(input())
+l=[]
 for i in range(N):
-    visited[i] = 1
-    dfs(i, i, 0, 1)
-    visited[i] = 0
-print(ans)
+  l.append(list(map(int,input().split())))
+
+q=[]
+visited=[False for i in range(N)]
+visited[0]=True
+for i in range(1,N):
+  if l[0][i]==0:
+    continue
+  visited[i]=True
+  heapq.heappush(q,[l[0][i],i,1,list(visited)])
+  visited[i]=False
+
+while q:
+  value,now,cnt,visited=heapq.heappop(q)
+  # print(value,now,cnt,visited,q)
+  if cnt==N:
+    print(value)
+    exit(0)
+
+  if cnt==N-1:
+    if l[now][0]!=0:
+      heapq.heappush(q,[value+l[now][0],0,cnt+1,list(visited)])
+    continue
+
+  for i in range(1,N):
+    if visited[i] or l[now][i]==0:
+      continue
+    visited[i]=True
+    heapq.heappush(q,[value+l[now][i],i,cnt+1,list(visited)])
+    visited[i]=False
+
+
+# 0 1 100 100
+# 100 0 1 100
+# 100 100 0 1
+# 1 0 100 100
+
+# 3
+# 0 10 15
+# 5 0 9
+# 6 13 0
