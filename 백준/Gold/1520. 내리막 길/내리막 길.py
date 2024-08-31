60d6c266beb4a977258405917715d780f6176e73
Mina Seo <96722691+esthel7@users.noreply.github.com>
2024-09-01 01:52:00 +0900

    [Gold III] Title: 내리막 길, Time: 156 ms, Memory: 45532 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.py
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -1,39 +1,40 @@
 import sys
 input=sys.stdin.readline
 
-# 상하좌우 이웃한 곳끼리 이동 가능
-# 왼쪽 위에서 오른쪽 아래로 이동
-# 높이가 더 낮은 지점으로만 이동
-
-def update(x,y,plus):
-  for i in range(4):
-    newX=x+xbox[i]
-    newY=y+ybox[i]
-    if 0<=newX<M and 0<=newY<N and l[newX][newY]<l[x][y]:
-      cnt[newX][newY]+=plus
-      if visited[newX][newY]:
-        update(newX,newY,plus)
-
-
 M,N=map(int,input().split())
 l=[]
 for i in range(M):
   l.append(list(map(int,input().split())))
 
-cnt=[[0 for i in range(N)]for i in range(M)]
-cnt[0][0]=1
-
-visited=[[False for i in range(N)]for j in range(M)]
-visited[0][0]=True
-
 xbox=[-1,1,0,0]
 ybox=[0,0,-1,1]
+visited=[[False for i in range(N)]for j in range(M)]
+cnt=[[0 for i in range(N)]for j in range(M)]
+cnt[0][0]=1
 
-for x in range(M):
-  for y in range(N):
-    visited[x][y]=True
-    if cnt[x][y]==0:
+def update(x,y,plus):
+  cnt[x][y]+=plus
+  for i in range(4):
+    newX=x+xbox[i]
+    newY=y+ybox[i]
+    if 0<=newX<M and 0<=newY<N and l[x][y]>l[newX][newY]:
+      if not visited[newX][newY]:
+        cnt[newX][newY]+=plus
+      else:
+        update(newX,newY,plus)
+
+for i in range(M):
+  for j in range(N):
+    visited[i][j]=True
+    if cnt[i][j]==0:
       continue
-    update(x,y,cnt[x][y])
+    for k in range(4):
+      newX=i+xbox[k]
+      newY=j+ybox[k]
+      if 0<=newX<M and 0<=newY<N and l[i][j]>l[newX][newY]:
+        if not visited[newX][newY]:
+          cnt[newX][newY]+=cnt[i][j]
+        else:
+          update(newX,newY,cnt[i][j])
 
 print(cnt[M-1][N-1])
