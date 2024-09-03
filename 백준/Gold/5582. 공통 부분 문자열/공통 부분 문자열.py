d47c8a8c30eb35879d6e7437cecff938f75f785f
Mina Seo <96722691+esthel7@users.noreply.github.com>
2024-09-03 23:20:34 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 436 ms, Memory: 236260 KB -BaekjoonHub

--- 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.py
+++ 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.py
@@ -1,25 +1,21 @@
 import sys
 input=sys.stdin.readline
 
-a=input().rstrip()
-b=input().rstrip()
+a=list(input().rstrip())
+b=list(input().rstrip())
 
 A=len(a)
 B=len(b)
 
-dp=[[0 for i in range(A)] for j in range(B)]
 answer=0
-
-for i in range(B):
-  for j in range(A):
-    if b[i]==a[j]:
+dp=[[0 for i in range(B)]for j in range(A)]
+for i in range(A):
+  for j in range(B):
+    if a[i]==b[j]:
       if i!=0 and j!=0:
-        dp[i][j]=dp[i-1][j-1]+1
-        if answer<dp[i][j]:
-          answer=dp[i][j]
+        dp[i][j]+=dp[i-1][j-1]+1
+        answer=max(answer,dp[i][j])
       else:
         dp[i][j]=1
-        if answer==0:
-          answer=1
 
 print(answer)
