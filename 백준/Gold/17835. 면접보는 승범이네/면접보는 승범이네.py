f5557b85b6982f7c36729d67fea40c100fc4333b
Mina Seo <96722691+esthel7@users.noreply.github.com>
2024-09-04 23:28:17 +0900

    [Gold II] Title: 면접보는 승범이네, Time: 2648 ms, Memory: 119164 KB -BaekjoonHub

--- 백준/Gold/17835. 면접보는 승범이네/면접보는 승범이네.py
+++ 백준/Gold/17835. 면접보는 승범이네/면접보는 승범이네.py
@@ -1,44 +1,39 @@
 import sys
 import heapq
+from collections import deque
 input=sys.stdin.readline
-
-# 출발 노드를 설정
-# 각 노드의 최소비용을 저장
-# 방문하지않은 노드 중 비용 적은 노드 선택
-# 해당 노드 거쳐 특정 노드로 가는 경우 고려
 
 N,M,K=map(int,input().split())
 graph={}
 for i in range(M):
-  U,V,C=map(int,input().split())
-  if V in graph:
-    if U in graph[V]:
-      if C<graph[V][U]:
-        graph[V][U]=C
+  u,v,c=map(int,input().split())
+  if v in graph:
+    if u in graph[v]:
+      graph[v][u]=min(graph[v][u],c)
     else:
-      graph[V][U]=C
+      graph[v][u]=c
   else:
-    graph[V]={U:C}
+    graph[v]={u:c}
 
+l=list(map(int,input().split()))
+
+last=10000000000
+dis=deque([last for i in range(N+1)])
 q=[]
-placeList=list(map(int,input().split()))
-for p in placeList:
-  heapq.heappush(q,[0,p])
+for place in l:
+  dis[place]=0
+  heapq.heappush(q,[0,place])
 
-final=[-1 for i in range(N+1)]
 while q:
-  cnt,node=heapq.heappop(q)
-  if final[node]==-1 or final[node]>cnt:
-    final[node]=cnt
-    if node in graph:
-      for end in graph[node]:
-        heapq.heappush(q,[graph[node][end]+cnt,end])
+  cost,place=heapq.heappop(q)
+  if place not in graph or dis[place]!=cost:
+    continue
+  for node in graph[place]:
+    if dis[node]>cost+graph[place][node]:
+      dis[node]=cost+graph[place][node]
+      heapq.heappush(q,[cost+graph[place][node],node])
 
-answer=[final[1],1]
-for i in range(2,N+1):
-  if answer[0]<final[i]:
-    answer[0]=final[i]
-    answer[1]=i
-
-print(answer[1])
-print(answer[0])
+dis.popleft()
+Max=max(dis)
+print(dis.index(Max)+1)
+print(Max)
