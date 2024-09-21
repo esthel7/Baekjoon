06fffc30ae7f1ce87e6e64b443ad81819ddf7b93
Mina Seo <96722691+esthel7@users.noreply.github.com>
2024-09-21 17:33:09 +0900

    [Silver III] Title: 주유소, Time: 208 ms, Memory: 31120 KB, Score: 41 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -0,0 +1,23 @@
+import sys
+input=sys.stdin.readline
+
+N=int(input())
+dis=list(map(int,input().split()))
+price=list(map(int,input().split()))
+
+dp=[-1 for i in range(N)]
+dp[0]=0
+
+for i in range(N): 
+  distance=0
+  for j in range(i+1,N):
+    distance+=dis[j-1]
+    value=dp[i]+price[i]*distance
+    if dp[j]==-1:
+      dp[j]=value
+    elif dp[j]<value:
+      break
+    else:
+      dp[j]=value
+
+print(dp[N-1])
