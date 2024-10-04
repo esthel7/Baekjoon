185cadc32245fde0d34594ea35b5a5d33af49bf6
Mina Seo <96722691+esthel7@users.noreply.github.com>
2024-10-05 03:30:53 +0900

    [Gold III] Title: 앱, Time: 128 ms, Memory: 111692 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.py
+++ 백준/Gold/7579. 앱/앱.py
@@ -1,17 +1,54 @@
-n, m = map(int, input().split())
-memory = [0] + list(map(int, input().split()))
-cost = [0] + list(map(int, input().split()))
-length = sum(cost)+1
-dp = [[0 for _ in range(length)] for _ in range(n+1)]
-ans = 10001
+import sys
+input=sys.stdin.readline
 
-for i in range(1, n+1):
-    ci, mi = cost[i], memory[i]
-    for j in range(length):
-        dp[i][j] = dp[i-1][j]
-    for j in range(ci, length):
-        dp[i][j] = max(dp[i-1][j-ci] + mi, dp[i][j])
-        if dp[i][j] >= m:
-            ans = min(ans, j)
+N,M=map(int,input().split())
+m=list(map(int,input().split()))
+c=list(map(int,input().split()))
 
-print(ans)
+info={}
+answer=sum(c)
+for i in range(N):
+  if c[i]>=answer:
+    continue
+
+  newInfo={}
+  for key in info.keys():
+    newInfo[key]=info[key]
+
+  for key in info.keys():
+    if key+c[i]>=answer:
+      continue
+    if key+c[i] in info:
+      newInfo[key+c[i]]=max(info[key]+m[i],info[key+c[i]])
+    else:
+      newInfo[key+c[i]]=info[key]+m[i]
+    if newInfo[key+c[i]]>=M:
+      answer=min(answer,key+c[i])
+      newInfo.pop(key+c[i])
+
+  if c[i] in info:
+    if c[i] in newInfo:
+      newInfo[c[i]]=max(info[c[i]],m[i],newInfo[c[i]])
+    else:
+      newInfo[c[i]]=max(info[c[i]],m[i])
+  else:
+    if c[i] in newInfo:
+      newInfo[c[i]]=max(m[i],newInfo[c[i]])
+    else:
+      newInfo[c[i]]=m[i]
+
+  if newInfo[c[i]]>=M:
+    answer=min(answer,c[i])
+    newInfo.pop(c[i])
+
+  info=newInfo
+
+
+print(answer)
+
+
+# 19 20169
+# 240 2560 434 6 31 577 500 2715 2916 952 2490 258 1983 1576 3460 933 1660 2804 2584
+# 82 77 81 0 36 6 53 78 49 82 82 33 66 8 60 0 98 91 93
+
+# 484
