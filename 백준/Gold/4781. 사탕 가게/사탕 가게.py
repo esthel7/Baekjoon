f4fc1553dd1c793c7bf204074d3699d25c750fed
Mina Seo <96722691+esthel7@users.noreply.github.com>
2024-10-06 02:12:31 +0900

    [Gold IV] Title: 사탕 가게, Time: 1628 ms, Memory: 110876 KB -BaekjoonHub

--- 백준/Gold/4781. 사탕 가게/사탕 가게.py
+++ 백준/Gold/4781. 사탕 가게/사탕 가게.py
@@ -1,32 +1,25 @@
 import sys
-input = sys.stdin.readline
+input=sys.stdin.readline
 
 while True:
-    n, money = map(float,input().rstrip().rsplit())
-    n = int(n)
+  n,m=map(float,input().split())
+  if n==0:
+    break
 
-    if n==0 and money==0.00:
-        break
+  n=int(n)
+  m=int(m*100)
 
-    money = int(money * 100)
+  answer=-1
+  money=[0 for i in range(m+1)]
+  for i in range(n):
+    c,p=map(float,input().split())
+    c=int(c)
+    p=int(p*100+0.5)
+    if p>m:
+      continue
+    for j in range(p,m+1):
+      money[j]=max(money[j],money[j-p]+c)
+      answer=max(answer,money[j])
 
-    candies = []
-    for _ in range(n):
-        c, p = map(float,input().rstrip().rsplit())
-        candies.append([int(c),int(p * 100 + 0.5)])
+  print(answer)
 
-    #dp[i] = 돈이 i일때, 사탕 중 가장 높은 칼로리
-    dp = [0] * 10001
-
-    # candies[0]: 칼로리, candies[1]: 가격
-    for i in range(1,money+1):
-        for j in range(n):
-            curr_candy_c = candies[j][0]
-            curr_candy_m = candies[j][1]
-
-            if (i < curr_candy_m):
-                continue
-
-            dp[i] = max(dp[i], dp[i-curr_candy_m] + curr_candy_c)
-
-    print(dp[money])
