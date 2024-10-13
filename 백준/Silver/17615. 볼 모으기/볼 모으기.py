6e71ad773590b3f9ad64f91a50e3cb6532b001c5
Mina Seo <96722691+esthel7@users.noreply.github.com>
2024-10-13 18:33:15 +0900

    [Silver I] Title: 볼 모으기, Time: 84 ms, Memory: 108080 KB, Score: 15 point -BaekjoonHub

--- 백준/Silver/17615. 볼 모으기/볼 모으기.py
+++ 백준/Silver/17615. 볼 모으기/볼 모으기.py
@@ -7,18 +7,18 @@ l=list(input().rstrip())
 last=l[-1]
 for i in range(N-1,-1,-1):
   if l[i]==last:
-    l.pop()
+    N-=1
   else:
     break
 
-if not l:
+if not N:
   print(0)
   exit(0)
 
 R=0
 B=0
-for item in l:
-  if item=='R':
+for i in range(N):
+  if l[i]=='R':
     R+=1
   else:
     B+=1
