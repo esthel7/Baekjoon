8bf38eb58a12cade0c44543809f5d4f093fb1d47
Mina Seo <96722691+esthel7@users.noreply.github.com>
2024-10-20 20:55:08 +0900

    [Silver III] Title: 타노스, Time: 36 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.py
+++ 백준/Silver/20310. 타노스/타노스.py
@@ -2,6 +2,7 @@ import sys
 input=sys.stdin.readline
 
 S=list(input().rstrip())
+
 num0=0
 num1=0
 for item in S:
@@ -9,11 +10,24 @@ for item in S:
     num0+=1
   else:
     num1+=1
+num0//=2
+num1//=2
 
-answer=''
-for i in range(num0//2):
-  answer+='0'
-for i in range(num1//2):
-  answer+='1'
+cnt=0
+for i in range(len(S)-1,-1,-1):
+  if S[i]=='0':
+    S.pop(i)
+    cnt+=1
+    if cnt==num0:
+      break
 
-print(answer)
+cnt=0
+i=0
+while cnt<num1:
+  if S[i]=='1':
+    S.pop(i)
+    cnt+=1
+    continue
+  i+=1
+
+print(''.join(S))
