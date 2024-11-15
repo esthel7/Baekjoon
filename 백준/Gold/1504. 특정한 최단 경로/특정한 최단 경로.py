08c80bb5752c7c9a55e8e6eb3c0c2fb1523c2c14
Mina Seo <96722691+esthel7@users.noreply.github.com>
2024-11-16 00:51:30 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 416 ms, Memory: 42216 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
@@ -2,62 +2,50 @@ import sys
 import heapq
 input=sys.stdin.readline
 
-def find(start):
-  visit=[Last for i in range(N+1)]
+def find(start,line):
   q=[]
-  heapq.heappush(q,[0,start])
-  while q:
-    cost,node=heapq.heappop(q)
-    if visit[node]>cost:
-      visit[node]=cost
-      if node not in graph:
-        continue
-      for nextNode in graph[node]:
-        heapq.heappush(q,[graph[node][nextNode]+cost,nextNode])
-
-  if start not in graph:
-    graph[start]={}
   for i in range(1,N+1):
-    if i in graph[start]:
-      if graph[start][i]>visit[i]:
-        graph[start][i]=visit[i]
-    else:
-      graph[start][i]=visit[i]
+    if graph[start][i]!=0:
+      visited[line][i]=graph[start][i]
+      heapq.heappush(q,[graph[start][i],i])
+
+  while q:
+    cost,idx=heapq.heappop(q)
+    if visited[line][idx]!=cost:
+      continue
+    for i in range(1,N+1):
+      if graph[idx][i]!=0 and (visited[line][i]==0 or visited[line][i]>cost+graph[idx][i]):
+        visited[line][i]=cost+graph[idx][i]
+        heapq.heappush(q,[cost+graph[idx][i],i])
 
 
 N,E=map(int,input().split())
-Last=2000*(N+1)
-
-graph={}
+graph=[[0 for i in range(N+1)]for j in range(N+1)]
 for i in range(E):
-  start,end,cost=map(int,input().split())
-  if start in graph:
-    if end in graph[start]:
-      if graph[start][end]>cost:
-        graph[start][end]=cost
-    else:
-      graph[start][end]=cost
-  else:
-    graph[start]={end:cost}
-
-  if end in graph:
-    if start in graph[end]:
-      if graph[end][start]>cost:
-        graph[end][start]=cost
-    else:
-      graph[end][start]=cost
-  else:
-    graph[end]={start:cost}
-
+  a,b,c=map(int,input().split())
+  if a==b:
+    continue
+  graph[a][b]=c
+  graph[b][a]=c
 u,v=map(int,input().split())
 
-find(1)
-find(u)
-find(v)
+visited=[[0 for i in range(N+1)]for j in range(3)]
+find(1,0)
+find(u,1)
+find(v,2)
 
-value=min(graph[1][u]+graph[u][v]+graph[v][N],graph[1][v]+graph[v][u]+graph[u][N])
-if value<Last:
-  print(value)
-else:
-  print(-1)
+if (u==1 and v==N) or (u==N and v==1):
+  if visited[0][N]!=0:
+    print(visited[0][N])
+  else:
+    print(-1)
+  exit()
 
+answer=-1
+if visited[0][u]!=0 and visited[1][v]!=0 and visited[2][N]!=0:
+  answer=visited[0][u]+visited[1][v]+visited[2][N]
+
+if visited[0][u]!=0 and visited[1][v]!=0 and visited[2][N]!=0 and (answer==-1 or answer>visited[0][v]+visited[2][u]+visited[1][N]):
+  answer=visited[0][v]+visited[2][u]+visited[1][N]
+
+print(answer)
