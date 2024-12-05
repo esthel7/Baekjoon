ae962274566781fe73827c958fc99b254b0d7e0d
Mina Seo <96722691+esthel7@users.noreply.github.com>
2024-12-06 00:08:25 +0900

    [Silver I] Title: 효율적인 해킹, Time: 9440 ms, Memory: 146188 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
@@ -1,42 +1,32 @@
-from collections import deque
 import sys
-input = sys.stdin.readline
+input=sys.stdin.readline
 
-def bfs(i):
-    visited = [0] * (N + 1)
-    q = deque([i])
-    visited[i] = 1
-    cnt = 1
+N,M=map(int,input().split())
+info=[[]for i in range(N+1)]
+for i in range(M):
+  A,B=map(int,input().split())
+  info[B].append(A)
 
-    while q:
-        x = q.popleft()
+answer=0
+answers=[]
+for i in range(1,N+1):
+  visited=[False for i in range(N+1)]
+  visited[i]=True
+  cnt=1
+  q=[i]
+  while q:
+    node=q.pop()
+    for key in info[node]:
+      if not visited[key]:
+        visited[key]=True
+        cnt+=1
+        q.append(key)
+  if answer<cnt:
+    answer=cnt
+    answers=[i]
+  elif answer==cnt:
+    answers.append(i)
 
-        for nx in computer[x]:
-            if not visited[nx]:
-                q.append(nx)
-                visited[nx] = 1
-                cnt += 1
-
-    return cnt
-
-
-N, M = map(int, input().split())
-computer = [[] for _ in range(N+1)]
-
-
-for _ in range(M):
-    A, B = map(int,input().split())
-    computer[B].append(A)
-
-answer = []
-max_hack = 0
-for i in range(1, N+1):
-    result = bfs(i)
-    if max_hack < result:
-        max_hack = result
-        answer.clear()
-        answer.append(i)
-    elif max_hack == result:
-        answer.append(i)
-
-print(*answer)
+for item in answers:
+  print(item,end=' ')
+print()
