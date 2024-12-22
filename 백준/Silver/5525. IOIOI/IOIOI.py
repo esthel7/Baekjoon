a1185b9613ce066250936b2ecc674bb3abcf2a05
Mina Seo <96722691+esthel7@users.noreply.github.com>
2024-12-22 23:48:49 +0900

    [Silver I] Title: IOIOI, Time: 332 ms, Memory: 34368 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,11 +1,17 @@
 N = int(input())
 M = int(input())
 S = input()
-P = 'IO'*N + 'I'
-answer = 0
+answer, i, count = 0, 0, 0
 
-for i in range(M - len(P)+1):
-    if S[i] == 'I':
-        if S[i:i+len(P)] == P:
+while i < (M - 1):
+    if S[i:i+3] == 'IOI':
+        i += 2
+        count += 1
+        if count == N:
             answer += 1
+            count -= 1
+    else:
+        i += 1
+        count = 0
+
 print(answer)
