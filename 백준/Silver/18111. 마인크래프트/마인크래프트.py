44542207422af5a7f840c7a9bc1b86656c667573
Mina Seo <96722691+esthel7@users.noreply.github.com>
2024-12-24 03:43:51 +0900

    [Silver II] Title: 마인크래프트, Time: 128 ms, Memory: 110760 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.py
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -1,22 +1,38 @@
 import sys
+input=sys.stdin.readline
 
-N, M, B = map(int, sys.stdin.readline().split())
-ground = []
-for _ in range(N):
-    ground.extend(map(int, sys.stdin.readline().split()))
+N,M,B=map(int,input().split())
+info={}
+Max=-1
+Min=260
+for i in range(N):
+  now=list(map(int,input().split()))
+  for j in range(M):
+    if now[j]>Max:
+      Max=now[j]
+    if now[j]<Min:
+      Min=now[j]
+    if now[j] in info:
+      info[now[j]]+=1
+    else:
+      info[now[j]]=1
 
-time = [0 for i in range(257)] # time[k]에 땅높이가 k일때의 소요시간 저장
-height = 0
-for g in range(257):
-    block = B # 인벤토리에 남은 블록 수
-    for i in ground:
-        if i <= g: # i == g이면 g-i=0
-            time[g] += g - i
-            block -= g - i
-        else:
-            time[g] += 2 * (i - g)
-            block += i - g
-    if block >= 0 and time[g] <= time[height]: # 오름차순으로 순회하므로, 답이 여러 개 있을 때 그중에서 땅의 높이가 가장 높은 것을 저장하게 됨
-        height = g # 최소 소요 시간인 땅높이 저장
+answer=[-1,-1]
+for i in range(Min,Max+1):
+  have=B
+  time=0
+  for key in info.keys():
+    if key<i:
+      have-=(i-key)*info[key]
+      time+=(i-key)*info[key]
+    elif key>i:
+      have+=(key-i)*info[key]
+      time+=2*(key-i)*info[key]
+  if have<0:
+    break
+  if answer[0]>time or answer[0]==-1:
+    answer=[time,i]
+  elif answer[0]==time:
+    answer[1]=i
 
-print(time[height], height)
+print(*answer)
