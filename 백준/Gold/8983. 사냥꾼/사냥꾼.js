2bc53bc61fc9f70f295f5dea15169d90f91b3661
Mina Seo <96722691+esthel7@users.noreply.github.com>
2025-03-13 22:11:40 +0900

    [Gold IV] Title: 사냥꾼, Time: 624 ms, Memory: 73680 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8983. 사냥꾼/사냥꾼.js
+++ 백준/Gold/8983. 사냥꾼/사냥꾼.js
@@ -10,7 +10,7 @@ rl.on('line',(line)=>{
   input.push(line)
 }).on('close',()=>{
   const [M,N,L]=input[iter++].split(' ').map(item=>Number(item))
-  const shot=input[iter++].split(' ').map(item=>Number(item))
+  const shot=input[iter++].split(' ').map(item=>Number(item)).sort((a,b)=>a-b)
   const loc={}
   for(let i=0;i<N;i++){
     const [x,y]=input[iter++].split(' ').map(item=>Number(item))
@@ -18,29 +18,46 @@ rl.on('line',(line)=>{
     else loc[x]=[y]
   }
 
-  const keys=Object.keys(loc)
-  for(let i=0;i<keys.length;i++){
-    loc[keys[i]].sort((a,b)=>a-b)
-  }
-
   let answer=0
-  for(let i=0;i<M;i++){
-    const spot=shot[i]
-    const start=spot-L<=0?1:spot-L
-    const end=spot+L>=1000000000?1000000000:spot+L+1
-    let possibleY=L-(Math.abs(start-spot))
-    for(let j=start;j<end;j++){
-      if (j in loc){
-        while (loc[j].length>0){
-          if (loc[j][0]<=possibleY){
-            answer+=1
-            loc[j].shift()
-          } else break
-        }
+  const keys=Object.keys(loc).map(item=>Number(item))
+  for(let i=0;i<keys.length;i++){
+    const spot=keys[i]
+    loc[spot].sort((a,b)=>a-b)
+    if(loc[spot][0]>L) continue
+
+    let start=0
+    let end=M-1
+    let possibleY=-1
+    while(start<=end){
+      const mid=Math.floor((start+end)/2)
+      if (shot[mid]===spot){
+        possibleY=L
+        break
+      } else if (shot[mid]>spot){
+        if (spot+L>=shot[mid]) possibleY=possibleY>L-(shot[mid]-spot)?possibleY:L-(shot[mid]-spot)
+        end=mid-1
+      } else {
+        if (spot-L<=shot[mid]) possibleY=possibleY>L-(spot-shot[mid])?possibleY:L-(spot-shot[mid])
+        start=mid+1
       }
-      if (j>=spot) possibleY-=1
-      else possibleY+=1
     }
+    if (possibleY===-1||loc[spot][0]>possibleY) continue
+
+    start=0
+    end=loc[spot].length-1
+    let plus=0
+    while(start<=end){
+      const mid=Math.floor((start+end)/2)
+      if (loc[spot][mid]===possibleY){
+        plus=mid+1
+        break
+      } else if (loc[spot][mid]>possibleY) end=mid-1
+      else {
+        plus=mid+1
+        start=mid+1
+      }
+    }
+    answer+=plus
   }
 
   console.log(answer)
