1b324c979590d6eff97a24d3d8c8ff162f15f896
Mina Seo <96722691+esthel7@users.noreply.github.com>
2025-04-28 19:36:45 +0900

    [Gold V] Title: 강의실, Time: 336 ms, Memory: 59364 KB -BaekjoonHub

---
+++ 백준/Gold/1374. 강의실/강의실.py
@@ -0,0 +1,17 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+N = int(input())
+l = [list(map(int, input().split())) for _ in range(N)]
+l.sort(key=lambda x: x[1])
+
+mh = []
+cnt = 0
+for i in l:
+    while mh and mh[0] <= i[1]: # 가장 일찍 끝나는 시간보다 시작 시간이 크면
+        heapq.heappop(mh)       # mh에서 가장 작은 원소 pop & return
+    heapq.heappush(mh, i[2])    # mh에 i[2]=끝나는 시간을 추가
+    cnt = max(cnt, len(mh))
+
+print(cnt)
