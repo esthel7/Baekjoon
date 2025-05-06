02e91bcfd95fedb54a3705a39a341fdb0307d9dd
Mina Seo <96722691+esthel7@users.noreply.github.com>
2025-05-06 22:24:08 +0900

    [Gold III] Title: 개구리 점프, Time: 708 ms, Memory: 84288 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/17619. 개구리 점프/개구리 점프.js
+++ 백준/Gold/17619. 개구리 점프/개구리 점프.js
@@ -18,50 +18,25 @@ rl.on('line',(line)=>{
   l.sort((a,b)=>a[0]-b[0])
   
   const match=[...new Array(N)]
-  const visit=[...new Array(N)].map(item=>{return{}})
+  const root=[...new Array(N)].map((item,idx)=>idx)
   for(let i=0;i<N;i++){
     const [x1,x2,y,idx]=l[i]
     match[idx]=i
     for(let j=i+1;j<N;j++){
       const [nx1,nx2,ny,nidx]=l[j]
-      if (nx1<=x2) {
-        visit[i][j]=true
-        visit[j][i]=true
-      } else break
+      if (nx1<=x2) root[j]=root[i]
+      else break
     }
   }
-
-  const keys=[]
-  for(let i=0;i<N;i++){
-    keys.push([...Object.keys(visit[i])])
-  }
-
-  function find(a,b){
-    const check=[...new Array(N)].map(item=>false)
-    check[a]=true
-    const q=[]
-    for(let i=0;i<keys[a].length;i++){
-      q.push(keys[a][i])
-      check[keys[a][i]]=true
-    }
-    while(q.length>0){
-      const now=q.shift()
-      if (now==b) return true
-      for(let i=0;i<keys[now].length;i++){
-        if (check[keys[now][i]]) continue
-        check[keys[now][i]]=true
-        q.push(keys[now][i])
-      }
-    }
-    return false
-  }
+  // console.log(l)
+  // console.log(root)
 
   const answer=[]
   for(let i=0;i<Q;i++){
     let [a,b]=input[iter++].split(' ').map(item=>Number(item))
     a=match[a]
     b=match[b]
-    if (find(a,b)) answer.push(1)
+    if (root[a]===root[b]) answer.push(1)
     else answer.push(0)
   }
   console.log(answer.join('\n'))
