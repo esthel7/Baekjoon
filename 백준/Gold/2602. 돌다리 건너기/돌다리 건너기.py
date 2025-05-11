1085c3a6dcea16e7710075db926caa1685884669
Mina Seo <96722691+esthel7@users.noreply.github.com>
2025-05-11 22:36:57 +0900

    [Gold IV] Title: 돌다리 건너기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/2602. 돌다리 건너기/돌다리 건너기.py
@@ -0,0 +1,36 @@
+order_list = input().strip()
+board_list = [input().strip() for _ in range(2)]
+
+order_len = len(order_list)
+board_len = len(board_list[0])
+
+def make_dp() :
+  dp = [[[0]*2 for _ in range(order_len)] for _ in range(board_len)]
+
+  for i in range(board_len) :
+    for j in range(order_len) :
+      for k in range(2) :
+        if board_list[k][i] == order_list[j] :
+          if j == 0 :
+            dp[i][j][k] = 1
+          else :
+            for l in range(i) :
+              dp[i][j][k] += dp[l][j-1][1-k]
+
+  return dp
+
+def find_result(dp) :
+  result = 0
+  for i in range(board_len) :
+    for j in range(2) :
+      result += dp[i][-1][j]
+
+  return result
+
+def solve() :
+  dp = make_dp()
+  result = find_result(dp)
+
+  print(result)
+
+solve()
