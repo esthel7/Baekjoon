12ca04b92e36bf715fc0ed647f0656a0b84eb173
Mina Seo <96722691+esthel7@users.noreply.github.com>
2025-10-13 17:02:05 +0900

    [Gold IV] Title: 호숫가의 개미굴, Time: 496 ms, Memory: 71896 KB, Score: 74 point -BaekjoonHub

--- 백준/Gold/28325. 호숫가의 개미굴/호숫가의 개미굴.js
+++ 백준/Gold/28325. 호숫가의 개미굴/호숫가의 개미굴.js
@@ -11,32 +11,39 @@ rl.on('line',(line)=>{
 }).on('close',()=>{
   const N=Number(input[iter++])
   const C=input[iter++].split(' ').map(item=>Number(item))
-  let start=0
+  const room=[]
   for(let i=0;i<N;i++){
-    if (C[i]>0) {
-      start=i
-      break
-    }
+    room.push([C[i],i])
   }
-  if (start===0 && C[0]===0) {
-    console.log(Math.floor(N/2))
+  room.sort((a,b)=>b[0]-a[0])
+  
+  const exist=[]
+  let answer=0
+  let move=0
+  while(move<N){
+    const [len,idx]=room[move++]
+    if (len===0) break
+    exist.push(idx)
+    answer+=len
+  }
+  exist.sort((a,b)=>a-b)
+
+  if (exist.length<=1){
+    console.log(answer+Math.floor(N/2))
     process.exit()
   }
-  let last=-2
-  let answer=0
-  let first=false
-  for(let i=start;i<start+N-1;i++){
-    if (C[i%N]>0) answer+=C[i%N]
-    else {
-      if (i===start) first=true
-      if (last+1!==i) {
-        last=i
-        answer+=1
-      }
-    }
+
+  // 겹치지 않게 사이에 추가하기
+  for(let i=1;i<exist.length;i++){
+    const first=exist[i-1]
+    const second=exist[i]
+    answer+=Math.ceil((second-first-1)/2)
   }
-  if (C[(start+N-1)%N]>0) answer+=C[(start+N-1)%N]
-  else if (last!==start+N-2 && !first) answer+=1
+  answer+=Math.floor((exist[0]+N-exist[exist.length-1])/2)
+
   console.log(answer)
   process.exit()
 })
+
+// 연결된 방 또는 쪽방에 아무도 없어야함
+// 1번에 들어있나 없나로 구분
